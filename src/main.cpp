7e16b20c033d5d2b6d09eb56e5c1cb11b3e4a9a7
Claudia Lagos <claudia.lagos@icrar.org>
2017-09-20 14:01:17 +0800

    Mofify CMakeLists to get the code to run while working writing routine

--- src/main.cpp
+++ src/main.cpp
@@ -220,10 +220,10 @@ int run(int argc, char **argv) {
 		//do_stuff_at_halo_level(all_halos_this_snapshot);
 
 //		/*write snapshots only if the user wants outputs at this time.*/
-		if(std::find(exec_params.output_snapshots.begin(), exec_params.output_snapshots.end(), snapshot) != exec_params.output_snapshots.end() )
+		/*if(std::find(exec_params.output_snapshots.begin(), exec_params.output_snapshots.end(), snapshot) != exec_params.output_snapshots.end() )
 		{
 			//write_output(snapshot, all_halos_this_snapshot);
-		}
+		}*/
 
 		destroy_galaxies_this_snapshot(all_halos_this_snapshot);
 
