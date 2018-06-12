611bab1faa419287967c803aba6fc91e1ee47360
Rodrigo Tobar <rtobar@icrar.org>
2018-06-12 16:21:13 +0800

    Travis-enabled shark
    The travis builds we are setting up here require gcc 5 or greater. This
    is because gcc did not fully implement some of the standard library (in
    particular the move operations for the iostream classes) until gcc5 and
    the corresponding libstdc++. Due to the same reason, when compiling
    against clang we install g++-7 (compiling against clang's own
    headers/libc++ is otherwise a bit difficult).
    
    On the MacOS side of things we don't have many surprises.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

---
+++ .travis/before_install.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+#
+# Travis CI before-install script
+#
+# ICRAR - International Centre for Radio Astronomy Research
+# (c) UWA - The University of Western Australia, 2018
+# Copyright by UWA (in the framework of the ICRAR)
+# All rights reserved
+#
+# Contributed by Rodrigo Tobar
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307  USA
+#
+
+cd ${TRAVIS_BUILD_DIR}
+
+# In MacOS we simply need to brew install some things
+if [ "${TRAVIS_OS_NAME}" = "osx" ]
+then
+
+	# The xcode7.3 osx image needs an update
+	if [ "${XCODE}" = "7.3" ]
+	then
+		brew update
+	fi
+
+	# The xcode 8.1 and 9.1 images need oclint to be uninstalled
+	# (see travis-ci issue #8826)
+	if [ "${XCODE}" = "8.1" -o "${XCODE}" = "9.1" ]
+	then
+		brew cask uninstall oclint
+	fi
+
+	# cxxtest pulls python@2, so we need to unlink
+	# the pre-installed python first
+	brew unlink python
+
+	# Minimal dependencies for testing
+	brew install gsl hdf5 cxxtest
+	return
+fi
+
+# Otherwise this is a linux box
+# We use cmake, but in trusty we still have 2.8...
+curl -O https://cmake.org/files/v3.2/cmake-3.2.3-Linux-x86_64.tar.gz
+tar -xf cmake-3.2.3-Linux-x86_64.tar.gz
+export PATH=${TRAVIS_BUILD_DIR}/cmake-3.2.3-Linux-x86_64/bin:$PATH
+export CMAKE_MODULE_PATH=${TRAVIS_BUILD_DIR}/cmake-3.2.3-Linux-x86_64/share/cmake-3.2/Modules:${CMAKE_MODULE_PATH}
+
+# Ubuntu Travis still comes with GSL 1.X but we need >= 2
+export GSL_ROOT_DIR=${TRAVIS_BUILD_DIR}/gsl/2.4
+export LD_LIBRARY_PATH=${GSL_ROOT_DIR}/lib:$LD_LIBRARY_PATH
+curl -O https://mirror.freedif.org/GNU/gsl/gsl-2.4.tar.gz
+tar xf gsl-2.4.tar.gz
+cd gsl-2.4
+./configure --prefix=${GSL_ROOT_DIR}
+make all -j2
+make install
+cd ..
---
+++ .travis/install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# Travis CI install script
+#
+# ICRAR - International Centre for Radio Astronomy Research
+# (c) UWA - The University of Western Australia, 2018
+# Copyright by UWA (in the framework of the ICRAR)
+# All rights reserved
+#
+# Contributed by Rodrigo Tobar
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307  USA
+#
+
+cd ${TRAVIS_BUILD_DIR}
+mkdir build
+cd build
+
+LIBPROFIT_CMAKE_OPTIONS="-DCMAKE_CXX_COMPILER=$COMPILER -DSHARK_TEST=ON -DGSL_ROOT_DIR=${GSL_ROOT_DIR}"
+
+# Go, go, go!
+cmake .. ${LIBPROFIT_CMAKE_OPTIONS}
+make all
