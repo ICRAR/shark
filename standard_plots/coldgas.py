074005fe4f9c5112acc9b92bd7de2d831ccea40a
Claudia Lagos <claudia.lagos@icrar.org>
2018-07-12 16:21:28 +0800

    Definition of early-type galaxy changed in coldgas.py

--- standard_plots/coldgas.py
+++ standard_plots/coldgas.py
@@ -58,6 +58,15 @@ def prepare_data(hdf5_data):
     h0log = np.log10(float(h0))
 
     n_typeg = len(typeg)
+    morpho_type = np.zeros(shape = (n_typeg))
+
+    # Early-type galaxies criterion of Khochfar et al. (2011).
+    ind = np.where(mbulge/(mdisk+mbulge) > 0.5)
+    morpho_type[ind] = 1.0
+    #ind = np.where((mbulge/(mdisk+mbulge) <= 0.5) & ((mgas + mgas_bulge)/(mdisk + mbulge) < 0.001))
+    #morpho_type[ind] = 1.0
+
+
     mh2_gals = np.zeros(shape = (2, n_typeg))
     mh1_gals = np.zeros(shape = (2, n_typeg))
     mgas_gals = np.zeros(shape = (2, n_typeg))
@@ -70,7 +79,7 @@ def prepare_data(hdf5_data):
     mh1_gals_etg = np.zeros(shape = (2, n_typeg))
     mgas_gals_etg = np.zeros(shape = (2, n_typeg))
 
-    ind = np.where((mdisk + mbulge > 0) & (mgas + mgas_bulge > 0) & (mH2 + mH2_bulge > 0) & (mbulge/(mdisk+mbulge) < 0.5))
+    ind = np.where((mdisk + mbulge > 0) & (mgas + mgas_bulge > 0) & (mH2 + mH2_bulge > 0) & (morpho_type == 0))
     # Data we'll use later
     mass = mdisk[ind] + mbulge[ind]
     mgas_gals_ltg[0,ind] = mh1_gals_ltg[0,ind] = mh2_gals_ltg[0,ind] = np.log10(mass) - h0log
@@ -82,7 +91,7 @@ def prepare_data(hdf5_data):
     mh1_relation_ltg = bin_it(x=mh1_gals_ltg[0, ind], y=mh1_gals_ltg[1, ind])
     mh2_relation_ltg = bin_it(x=mh2_gals_ltg[0, ind], y=mh2_gals_ltg[1, ind])
 
-    ind = np.where((mdisk + mbulge > 0) & (mgas + mgas_bulge > 0) & (mH2 + mH2_bulge > 0) & (mbulge/(mdisk+mbulge) >= 0.5))
+    ind = np.where((mdisk + mbulge > 0) & (mgas + mgas_bulge > 0) & (mH2 + mH2_bulge > 0) & (morpho_type == 1))
     # Data we'll use later
     mass = mdisk[ind] + mbulge[ind]
     mgas_gals_etg[0,ind] = mh1_gals_etg[0,ind] = mh2_gals_etg[0,ind] = np.log10(mass) - h0log
