65a6943c39f2ea1c338f8bb191d099030cef84e4
Rodrigo Tobar <rtobar@icrar.org>
2018-07-12 12:11:03 +0800

    Bulk change of license notice in files
    Now all source code files include the GPL3+ license notice, with
    copyright to ICRAR/UWA and the year they were initially created.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- src/gas_cooling.cpp
+++ src/gas_cooling.cpp
@@ -1,8 +1,24 @@
-/*
- * gas_cooling.cpp
- *
- *  Created on: 17May,2017
- *      Author: clagos
+//
+// ICRAR - International Centre for Radio Astronomy Research
+// (c) UWA - The University of Western Australia, 2017
+// Copyright by UWA (in the framework of the ICRAR)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+/**
+ * @file
  */
 
 #include <cmath>
