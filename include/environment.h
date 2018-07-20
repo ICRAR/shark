fed9e57831021f16231a385d682aa4341190f9c7
Claudia Lagos <claudia.lagos@icrar.org>
2018-07-20 17:37:18 +0800

    New class and functions of environmental processes
    - New environment class that contains all the physical processes
    related to environmental effects. The current model is very simple though
    allowing only instantaneous stripping of the hot gas or not stripping
    at all. The latter is controlled by a boolean parameter input by the user
    in the config file, stripping.
    
    - Source file added to CmakeLists for compilation

---
+++ include/environment.h
@@ -0,0 +1,72 @@
+//
+// ICRAR - International Centre for Radio Astronomy Research
+// (c) UWA - The University of Western Australia, 2018
+// Copyright by UWA (in the framework of the ICRAR)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+/**
+ * @file
+ */
+
+#ifndef INCLUDE_ENVIRONMENT_H_
+#define INCLUDE_ENVIRONMENT_H_
+
+
+
+#include <memory>
+#include <utility>
+
+#include "components.h"
+#include "dark_matter_halos.h"
+#include "options.h"
+
+namespace shark {
+
+class EnvironmentParameters{
+
+public:
+	EnvironmentParameters(const Options &options);
+
+	bool gradual_stripping = false;
+	bool stripping = true;
+
+};
+
+class Environment{
+
+public:
+	Environment(const EnvironmentParameters &parameters);
+
+	void process_satellite_subhalo_environment (Subhalo &satellite_subhalo, Subhalo &central_subhalo);
+
+private:
+
+	EnvironmentParameters parameters;
+};
+
+typedef std::shared_ptr<Environment> EnvironmentPtr;
+
+template <typename ...Ts>
+EnvironmentPtr make_environment(Ts&&...ts)
+{
+	return std::make_shared<Environment>(std::forward<Ts>(ts)...);
+}
+
+
+}//end namespace shark
+
+
+#endif /* INCLUDE_ENVIRONMENT_H_ */
---
+++ src/environment.cpp
@@ -0,0 +1,66 @@
+//
+// ICRAR - International Centre for Radio Astronomy Research
+// (c) UWA - The University of Western Australia, 2018
+// Copyright by UWA (in the framework of the ICRAR)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+
+#include <cmath>
+
+#include "environment.h"
+
+namespace shark {
+
+EnvironmentParameters::EnvironmentParameters(const Options &options)
+{
+	options.load("environment.gradual_stripping", gradual_stripping);
+	options.load("environment.stripping", stripping);
+
+}
+
+Environment::Environment(const EnvironmentParameters &parameters):
+	parameters(parameters){
+	//no-opt
+}
+
+void Environment::process_satellite_subhalo_environment(Subhalo &satellite_subhalo, Subhalo &central_subhalo){
+
+	if(parameters.stripping){
+		// Ejected gas is moved to the budget of ejected gas of the central, as this gas escaped
+		// the subhalo of the satellite.
+		central_subhalo.ejected_galaxy_gas += satellite_subhalo.ejected_galaxy_gas;
+		satellite_subhalo.ejected_galaxy_gas.restore_baryon();
+
+		if(parameters.gradual_stripping){
+			//TODO
+		}
+		else{
+			if(satellite_subhalo.hot_halo_gas.mass > 0 or satellite_subhalo.ejected_galaxy_gas.mass > 0 or satellite_subhalo.cold_halo_gas.mass > 0){
+
+				central_subhalo.hot_halo_gas += satellite_subhalo.hot_halo_gas;
+				central_subhalo.hot_halo_gas += satellite_subhalo.cold_halo_gas;
+
+				satellite_subhalo.hot_halo_gas.restore_baryon();
+				satellite_subhalo.cold_halo_gas.restore_baryon();
+			}
+		}
+	}
+
+}
+
+}
+
+
