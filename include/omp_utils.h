22a8b59a1c067e19da891b7c3f1e7384dd7052cf
Rodrigo Tobar <rtobar@icrar.org>
2018-08-08 17:25:30 +0800

    New OpenMP-related utilities header file
    This header file contains two functions: omp_static_for and
    omp_dynamic_for. These are templated, and represent the two types of
    OpenMP for loops we have been using until now. They use either a
    [start,end) integer range over which parallelization should occur, or a
    container supporting random iterators which is then looped from
    [0,size). In both cases a callable is provided that should be called
    with individual elements coming out from the iteration.
    
    Passing down the full container instead of first/last iterators is
    necessary if we want to support older OpenMP versions. Only in OpenMP
    3.0 one can pass a random iterator to the loop construct; before that
    the variable in the loop construct could only be an integer.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

---
+++ include/omp_utils.h
@@ -0,0 +1,156 @@
+//
+// ICRAR - International Centre for Radio Astronomy Research
+// (c) UWA - The University of Western Australia, 2018
+// Copyright by UWA (in the framework of the ICRAR)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+/**
+ * @file
+ *
+ * OpenMP-related utilities. These allow users to easily write "parallel for"
+ * constructs without having to deal with the OpenMP pragmas themselves, and
+ * ensuring they don't break compatibility with the minimum supported version
+ * of OpenMP we ensure with shark.
+ *
+ * At the moment, we offer compatibility with OpenMP 2.0. This is a pretty low
+ * version (5.0 is about to come out as of this writing), but on the one hand
+ * we don't really need more at the moment, and it also means we can hit more
+ * compilers.
+ */
+
+#ifndef SHARK_OMP_UTILS_H_
+#define SHARK_OMP_UTILS_H_
+
+#include <type_traits>
+
+#include "config.h"
+
+#ifdef SHARK_OPENMP
+#include <omp.h>
+#endif // SHARK_OPENMP
+
+namespace shark {
+
+namespace detail {
+
+inline int thread_index()
+{
+#ifdef SHARK_OPENMP
+		return omp_get_thread_num();
+#else
+		return 0;
+#endif // SHARK_OPENMP
+}
+
+}  // namespace detail
+
+/**
+ * Utility function to call a function over individual members of a container
+ * using an OpenMP parallel for with static scheduling. Using this function is
+ * simpler than having to repeat this code, and less error prone. If OpenMP
+ * support is not present then no parallelization takes place.
+ *
+ * @param first The first number of the range
+ * @param last The last (exclusive) number of the range
+ * @param num_threads The number of threads to use for parallelization
+ * @param f A callable that takes a single number from the range @p [first,last)
+ * and the thread index under which it is being executed
+ */
+template <typename Integer, typename Callable>
+void omp_static_for(Integer first, Integer last, int num_threads, Callable &&f)
+{
+	typedef typename std::make_signed<Integer>::type omp_iterator_t;
+	omp_iterator_t __first = first;
+	omp_iterator_t __last = last;
+#ifdef SHARK_OPENMP
+	#pragma omp parallel for num_threads(num_threads) schedule(static)
+#endif // SHARK_OPENMP
+	for (omp_iterator_t it = __first; it < __last; it++) {
+		f(it, detail::thread_index());
+	}
+}
+
+/**
+ * Like omp_static_for<Integer, Callable>(Integer, Integer, int, Callable),
+ * but takes a container reference instead of two integers, and iterates over
+ * the elements of the container. The container's iterator needs to be a random
+ * iterator.
+ *
+ * @param container A container of elements (set, vector, etc.)
+ * @param num_threads The number of threads to use for parallelization
+ * @param f A callable that takes a single item from the container and the
+ * thread index under which it is being executed
+ */
+template <typename Container, typename Callable>
+void omp_static_for(Container &&container, int num_threads, Callable &&f)
+{
+	auto size = container.size();
+	typedef typename std::make_signed<decltype(size)>::type signed_t;
+	omp_static_for(std::size_t(0), container.size(), num_threads, [&](signed_t i, int thread_num){
+		f(container[i], thread_num);
+	});
+}
+
+/**
+ * Like omp_static_for<Integer, Callable>(Integer, Integer, int, Callable),
+ * but using dynamic scheduling with a given chunk size.
+ *
+ * @param first The first number of the range
+ * @param last The last (exclusive) number of the range
+ * @param num_threads The number of threads to use for parallelization
+ * @param chunk The chunk size for the dynamic scheduling
+ * @param f A callable that takes a single number from the range @p [first,last)
+ * and the thread index under which it is being executed
+ */
+template <typename Integer, typename Callable>
+void omp_dynamic_for(Integer first, Integer last, int num_threads, int chunk, Callable &&f)
+{
+	typedef typename std::make_signed<Integer>::type omp_iterator_t;
+	omp_iterator_t __first = first;
+	omp_iterator_t __last = last;
+#ifdef SHARK_OPENMP
+	#pragma omp parallel for num_threads(num_threads) schedule(dynamic, chunk)
+#endif // SHARK_OPENMP
+	for (omp_iterator_t it = __first; it < __last; it++) {
+		f(it, detail::thread_index());
+	}
+}
+
+/**
+ * Like omp_dynamic_for<Integer, Callable>(Integer, Integer, int, int, Callable),
+ * but takes a container reference instead of two integers, and iterates over
+ * the elements of the container. The container's iterator needs to be a random
+ * iterator.
+ *
+ * @param container A container of elements (set, vector, etc.)
+ * @param num_threads The number of threads to use for parallelization
+ * @param chunk The chunk size for the dynamic scheduling
+ * @param f A callable that takes a single item from the container and the
+ * thread index under which it is being executed
+ */
+template <typename Container, typename Callable>
+void omp_dynamic_for(Container &&container, int num_threads, int chunk, Callable &&f)
+{
+	auto size = container.size();
+	typedef typename std::make_signed<decltype(size)>::type signed_t;
+	omp_dynamic_for(std::size_t(0), container.size(), num_threads, chunk, [&](signed_t i, int thread_num){
+		f(container[i], thread_num);
+	});
+}
+
+}  // namespace shark
+
+#endif /* SHARK_OMP_UTILS_H_ */
