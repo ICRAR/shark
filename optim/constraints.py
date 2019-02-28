26d0afbf0ee6fc5b6e1c0ba243dc93d3cb0489fe
Rodrigo Tobar <rtobar@icrar.org>
2019-02-28 18:25:54 +0800

    Adding license statement
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- optim/constraints.py
+++ optim/constraints.py
@@ -1,6 +1,26 @@
-#Runs shark from python, just a test to make sure I knew how
-# exists within the code directory at the highest level
-#imports
+#
+# ICRAR - International Centre for Radio Astronomy Research
+# (c) UWA - The University of Western Australia, 2019
+# Copyright by UWA (in the framework of the ICRAR)
+#
+# Originally contributed by Mawson Sammons
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+"""
+Constraints for optimizers to evaluate shark models against observations
+"""
 
 import os
 
