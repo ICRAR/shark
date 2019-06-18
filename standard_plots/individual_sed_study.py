16b8c74832142cf945edb67564dac88f066cecb6
Claudia Lagos <claudia.lagos@icrar.org>
2019-06-18 17:33:31 +0800

    Updated individual_sed_study.py for variable ext
    Forgot to include the individual_sed_study.py in the commit of updates to
    variable extinction model

--- standard_plots/individual_sed_study.py
+++ standard_plots/individual_sed_study.py
@@ -146,7 +146,7 @@ def plot_individual_seds_z2(plt, outdir, obsdir, h0, SEDs_dust_z2, total_sfh_z2,
     #subplots = (331, 332, 333, 334, 335, 336, 337, 338, 339)
     subplots = (311, 312, 313)
     indices  = (0, 1, 2) #, 6, 7, 8)
-    sfrs     = (1000.0, 100, 10.0)
+    sfrs     = (500.0, 100, 10.0)
     labelsfrs=  ('$\\rm SFR/M_{\odot} yr^{-1}= 1000$', '$\\rm SFR/M_{\odot} yr^{-1}= 100$', '$\\rm SFR/M_{\odot} yr^{-1}= 10$')
 
     for subplot, idx in zip(subplots, indices):
@@ -167,10 +167,9 @@ def plot_individual_seds_z2(plt, outdir, obsdir, h0, SEDs_dust_z2, total_sfh_z2,
         ind = np.where((gal_props_z2[:,3] > sfrs[idx] - 0.2*sfrs[idx]) & (gal_props_z2[:,3] < sfrs[idx] + 0.2*sfrs[idx]) & (gal_props_z2[:,1] > 1e9))
         if(len(gal_props_z2[ind,3]) > 0):
            age_selec= gal_props_z2[ind,0]
-
-           selecgal = 0 #len(age_selec[0])/2
-           if idx > 0:
-              selecgal = 2
+           selecgal = len(age_selec)/2
+           #if idx > 0:
+           #   selecgal = 2
            #choose first galaxy
            SEDs_selec   = SEDs_dust_z2[ind,4,:]
            SEDs_selec_b = SEDs_dust_z2[ind,2,:]
@@ -179,7 +178,6 @@ def plot_individual_seds_z2(plt, outdir, obsdir, h0, SEDs_dust_z2, total_sfh_z2,
            tot_sfh_selec  = total_sfh_z2[ind,:]
            disk_sfh_selec = disk_sfh_z2[ind,:]
            sb_sfh_selec   = sb_sfh_z2[ind,:]
-
            SED_in   = pow(10.0, (SEDs_selec[0,selecgal] + 48.6)/(-2.5)) 
            SED_in_d = pow(10.0, (SEDs_selec_d[0,selecgal] + 48.6)/(-2.5)) 
            SED_in_b = pow(10.0, (SEDs_selec_b[0,selecgal] + 48.6)/(-2.5)) 
@@ -189,9 +187,9 @@ def plot_individual_seds_z2(plt, outdir, obsdir, h0, SEDs_dust_z2, total_sfh_z2,
            yplotb = np.log10(SED_in_b * freq_bands)
 
            xplot = np.log10(lambda_bands) 
-           ax.plot(xplot,yplot,'kx')
-           ax.plot(xplot,yplotd,'bx')
-           ax.plot(xplot,yplotb,'rx')
+           ax.plot(xplot,yplot,'ks', alpha=0.5)
+           ax.plot(xplot,yplotd,'bd', alpha=0.5)
+           ax.plot(xplot,yplotb,'rD', alpha=0.5)
 
            sfh_in  = tot_sfh_selec[0,selecgal]
            sfh_ind = disk_sfh_selec[0,selecgal]
@@ -213,6 +211,53 @@ def plot_individual_seds_z2(plt, outdir, obsdir, h0, SEDs_dust_z2, total_sfh_z2,
            ax.plot(m*LBT_z2 + b, np.log10(sfh_ind), 'b', linestyle='dotted')
 
     common.savefig(outdir, fig, "SEDs_SFRs_z2_threepanel.pdf")
+
+    #wavelength in angstroms.
+    xmin, xmax, ymin, ymax = 3.0, 7.0, -1, 6.8
+    xleg = xmin + 0.1 * (xmax-xmin)
+    yleg = ymax - 0.07 * (ymax-ymin)
+
+    fig = plt.figure(figsize=(6,4.5))
+    sfrs     = (100.0, 100, 10.0)
+    colors   = ('Indigo','purple','Navy','DarkTurquoise', 'Aquamarine', 'Green','PaleGreen','GreenYellow','Gold','Yellow','Orange','OrangeRed','red','DarkRed','FireBrick','Crimson','IndianRed','LightCoral','Maroon','brown','Sienna','SaddleBrown','Chocolate','Peru','DarkGoldenrod','Goldenrod','SandyBrown')
+    linestyles = ('dotted', 'dashed', 'solid')
+    greys = ('Black','DarkSlateGray','DimGray')
+    idx = 0
+    ax = fig.add_subplot(111)
+    common.prepare_ax(ax, xmin, xmax, ymin, ymax, xtit, ytit, locators=(2, 2, 1, 1))
+    plt.subplots_adjust(bottom = 0.2)
+    #ax.text(xleg,yleg, '$\\rm SFR>250 M_{\\odot} yr^{-1}$')
+    ax.text(3,6.9, '13.6', fontsize=12)
+    ax.text(4.7,7.1, 'LBT/Gyr', fontsize=12)
+    ax.text(6.9,6.9, '10.6', fontsize=12)
+    ax.text(7.1, 5.3, '$\\rm log_{10}(SFR/M_{\odot} yr^{-1})$', rotation='vertical', fontsize=12)
+
+    ind = np.where((gal_props_z2[:,3] > 255) & (gal_props_z2[:,1] > 1e9))
+    print gal_props_z2[ind,3],gal_props_z2[ind,1]
+    age_selec= gal_props_z2[ind,0]
+    SEDs_selec   = SEDs_dust_z2[ind,4,:]
+    tot_sfh_selec  = total_sfh_z2[ind,:]
+    ssfrs = np.log10(gal_props_z2[ind,3]/gal_props_z2[ind,1]*1e9)
+    ngals = 3
+    for g in range(0,ngals):
+        SED_in   = pow(10.0, (SEDs_selec[0,g] + 48.6)/(-2.5)) 
+        yplot  = np.log10(SED_in * freq_bands)
+        xplot = np.log10(lambda_bands) 
+        ax.plot(xplot,yplot,color=greys[g],linestyle=linestyles[g], linewidth=1) 
+        for xi,yi,c in zip(xplot,yplot,colors):
+            ax.plot(xi,yi, 'd', markersize=4, color=c, alpha=0.5)
+        sfh_in  = tot_sfh_selec[0,g]
+        lowsfr = np.where(sfh_in < 0.01)
+        sfh_in[lowsfr] = 0.001
+ 
+        maxLBT = max(LBT_z2)
+        minLBT = min(LBT_z2)
+        m = 4.0/(minLBT - maxLBT)
+        b = 3.0 - m*maxLBT
+        ax.plot(m*LBT_z2 + b, np.log10(sfh_in), color=greys[g], linestyle=linestyles[g], label='$\\rm log_{10}(sSFR/Gyr^{-1})$=%.2f' % ssfrs[0,g])
+
+    common.prepare_legend(ax, greys, loc='center left')
+    common.savefig(outdir, fig, "SEDs_SFRs_z2_starbursts.pdf")
 
 
 def plot_individual_seds(plt, outdir, obsdir, h0, SEDs_dust_z0, SEDs_nodust_z0, total_sfh_z0, gal_props_z0, LBT):
@@ -276,8 +321,8 @@ def plot_individual_seds(plt, outdir, obsdir, h0, SEDs_dust_z0, SEDs_nodust_z0, 
            xplot = np.log10(lambda_bands) 
    
            for xi,yi,yind,c in zip(xplot,yplot,yplotnd,colors):
-               ax.plot(xi,yi, 'x', markersize=6, color=c)
-               ax.plot(xi,yind, 'x', markersize=3, color=c)
+               ax.plot(xi,yi, 'd', markersize=6, color=c, alpha=0.5)
+               ax.plot(xi,yind, 'd', markersize=3, color=c, alpha=0.5)
   
            sfh_in = tot_sfh_selec[0,selecgal]
            lowsfr = np.where(sfh_in < 0.01)
@@ -367,9 +412,12 @@ def plot_individual_seds(plt, outdir, obsdir, h0, SEDs_dust_z0, SEDs_nodust_z0, 
         ind = np.where((gal_props_z0[:,0] > ages[idx]-0.3) & (gal_props_z0[:,0] < ages[idx]+0.3) & (gal_props_z0[:,1] > 1e9))
         if(len(gal_props_z0[ind,0]) > 0):
            age_selec= gal_props_z0[ind,0]
-           selecgal = 0 #len(age_selec[0])/2
-           if idx == 3:
-              selecgal = 1
+           if(idx == 0 or idx == 2):
+              selecgal = len(age_selec[0])/5
+           else:
+              selecgal = len(age_selec[0])/4
+           #if idx == 3:
+           #   selecgal = 1
            #choose first galaxy
            SEDs_selec        = SEDs_dust_z0[ind,4,:]
            SEDs_nodust_selec = SEDs_nodust_z0[ind,4,:]
@@ -383,8 +431,8 @@ def plot_individual_seds(plt, outdir, obsdir, h0, SEDs_dust_z0, SEDs_nodust_z0, 
            xplot = np.log10(lambda_bands) 
    
            for xi,yi,yind,c in zip(xplot,yplot,yplotnd,colors):
-               ax.plot(xi,yi, 'x', markersize=6, color=c)
-               ax.plot(xi,yind, 'x', markersize=3, color=c)
+               ax.plot(xi,yi, 'd', markersize=6, color=c, alpha=0.2)
+               ax.plot(xi,yind, 'd', markersize=3, color=c)
   
            sfh_in = tot_sfh_selec[0,selecgal]
            lowsfr = np.where(sfh_in < 0.01)
@@ -414,22 +462,24 @@ def plot_individual_seds(plt, outdir, obsdir, h0, SEDs_dust_z0, SEDs_nodust_z0, 
         common.prepare_ax(ax, xmin, xmax, ymin, ymax, xtitle, ytitle, locators=(2, 2, 1, 1))
         ax.text(xleg,yleg, labelages[idx])
 
-        ind = np.where((gal_props_z0[:,0] > ages[idx]-0.3) & (gal_props_z0[:,0] < ages[idx]+0.3) & (gal_props_z0[:,1] > 1e9))
+        ind = np.where((gal_props_z0[:,0] > ages[idx]-0.3) & (gal_props_z0[:,0] < ages[idx]+0.3) & (gal_props_z0[:,1] > 1e9) & (gal_props_z0[:,4] <= 1))
         if(len(gal_props_z0[ind]) > 0):
-           age_selec= gal_props_z0[ind,0]
-           selecgal = len(age_selec[0])/2
+           age_selec     = gal_props_z0[ind,0]
            tot_sfh_selec = total_sfh_z0[ind,:]
            age_selec     = gal_props_z0[ind,0]
            typesg        = gal_props_z0[ind,4]
+           start         = len(age_selec[0])/2
            numgals       = 10
-           if(numgals > len(age_selec[0])):
+           if(numgals+start > len(age_selec[0])):
               numgals = len(age_selec[0])
- 
-           for gal in range(0,numgals): 
-               sfh_in = tot_sfh_selec[0,gal]
+              start = 0
+
+           for gal in range(0,numgals):
+               j = gal + start 
+               sfh_in = tot_sfh_selec[0,j]
                lowsfr = np.where(sfh_in < 0.01)
                sfh_in[lowsfr] = 0.001
-               if(typesg[0,gal] == 0):
+               if(typesg[0,j] == 0):
                   ax.plot(LBT, np.log10(sfh_in), color=colors[gal], linewidth=1)
                else:
                   ax.plot(LBT, np.log10(sfh_in), color=colors[gal], linewidth=1, linestyle = 'dashed')
@@ -437,8 +487,7 @@ def plot_individual_seds(plt, outdir, obsdir, h0, SEDs_dust_z0, SEDs_nodust_z0, 
 
     common.savefig(outdir, fig, "SFHs_ages_threepanel.pdf")
 
-
-def prepare_data(hdf5_data, sfh, phot_data, ids_sed, index, nbands):
+def prepare_data(hdf5_data, sfh, phot_data, phot_data_nod, index, nbands):
    
     #star_formation_histories and SharkSED have the same number of galaxies in the same order, and so we can safely assume that to be the case.
     #to select the same galaxies in galaxies.hdf5 we need to ask for all of those that have a stellar mass > 0, and then assume that they are in the same order.
@@ -455,8 +504,20 @@ def prepare_data(hdf5_data, sfh, phot_data, ids_sed, index, nbands):
     #2: total bulge
     #3: disk
     #4: total
-    SEDs_dust   = phot_data[:,1,0,:,:]
-    SEDs_nodust = phot_data[:,0,0,:,:]
+    ind = np.where(mdisk + mbulge > 0)
+    SEDs_dust = np.zeros(shape = (len(mdisk[ind]), 5, nbands))
+    SEDs_nodust = np.zeros(shape = (len(mdisk[ind]), 5, nbands))
+
+    p = 0
+    for c in range(0,5):
+        indust = phot_data[p]
+        innodust = phot_data_nod[p]
+        for i in range(0,nbands):
+            SEDs_dust[:,c,i] = indust[i,:]
+            SEDs_nodust[:,c,i] = innodust[i,:]
+        p = p + 1
+
+
     ngals       = len(SEDs_dust[:,0,0])
     nsnap       = len(bulge_diskins_hist[0,:])
     total_sfh = np.zeros(shape = (ngals, nsnap))
@@ -480,6 +541,8 @@ def prepare_data(hdf5_data, sfh, phot_data, ids_sed, index, nbands):
 
 def main(model_dir, outdir, redshift_table, subvols, obsdir):
 
+    Variable_Ext = True
+
     # Loop over redshift and subvolumes
     plt = common.load_matplotlib()
     fields = {'galaxies': ('mstars_disk', 'mstars_bulge', 'mvir_hosthalo',
@@ -490,6 +553,9 @@ def main(model_dir, outdir, redshift_table, subvols, obsdir):
                   'bulges_mergers': ('star_formation_rate_histories'),
                   'disks': ('star_formation_rate_histories')}
 
+    fields_sed = {'SED/ab_dust': ('bulge_d','bulge_m','bulge_t','disk','total'),}
+    fields_sed_nod = {'SED/ab_nodust': ('bulge_d','bulge_m','bulge_t','disk','total')}
+
     z = (0, 2) #0.5, 1, 1.5, 2, 3)
     snapshots = redshift_table[z]
 
@@ -498,21 +564,28 @@ def main(model_dir, outdir, redshift_table, subvols, obsdir):
 
         hdf5_data = common.read_data(model_dir, snapshot, fields, subvols)
         sfh, delta_t, LBT = common.read_sfh(model_dir, snapshot, sfh_fields, subvols)
-        seds, ids, nbands = common.read_photometry_data(model_dir, snapshot, subvols)
-    
-        (SEDs_dust, SEDs_nodust, total_sfh, sb_sfh, disk_sfh, gal_props) = prepare_data(hdf5_data, sfh, seds, ids, index, nbands)
+        if(Variable_Ext == False):
+           seds = common.read_photometry_data(model_dir, snapshot, fields_sed, subvols)
+           seds_nod = common.read_photometry_data(model_dir, snapshot, fields_sed_nod, subvols)
+        else:
+           seds = common.read_photometry_data_variable_tau_screen(model_dir, snapshot, fields_sed, subvols)
+           seds_nod = common.read_photometry_data_variable_tau_screen(model_dir, snapshot, fields_sed_nod, subvols)
+
+        nbands = len(seds[0])
+ 
+        (seds_dust, seds_nodust, total_sfh, sb_sfh, disk_sfh, gal_props) = prepare_data(hdf5_data, sfh, seds, seds_nod, index, nbands)
 
         h0, volh = hdf5_data[0], hdf5_data[1]
         if(index == 0):
-            SEDs_nodust_z0 = SEDs_nodust
-            SEDs_dust_z0 = SEDs_dust
+            SEDs_nodust_z0 = seds_nodust
+            SEDs_dust_z0 = seds_dust
             total_sfh_z0 = total_sfh
             gal_props_z0 = gal_props
             LBT_z0 = LBT
-            plot_individual_seds(plt, outdir, obsdir, h0, SEDs_dust_z0, SEDs_nodust, total_sfh_z0, gal_props_z0, LBT_z0)
+            plot_individual_seds(plt, outdir, obsdir, h0, SEDs_dust_z0, SEDs_nodust_z0, total_sfh_z0, gal_props_z0, LBT_z0)
 
         if(index == 1):
-            SEDs_dust_z2 = SEDs_dust
+            SEDs_dust_z2 = seds_dust
             total_sfh_z2 = total_sfh
             disk_sfh_z2  = disk_sfh
             sb_sfh_z2    = sb_sfh
