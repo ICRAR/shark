1c166b4ece1d2ee55cad74d2e906aaf1474ddf79
Rodrigo Tobar <rtobar@icrar.org>
2020-05-26 18:08:22 +0800

    Add gsl_utils.h header with gsl_invoke function
    This function internally invokes a GSL function and checks that its
    return code is GSL_SUCCESS. If it's not it throws a gsl_error exception.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

---
+++ include/gsl_utils.h
@@ -0,0 +1,53 @@
+//
+// ICRAR - International Centre for Radio Astronomy Research
+// (c) UWA - The University of Western Australia, 2020
+// Copyright by UWA (in the framework of the ICRAR)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+/**
+ * @file
+ */
+
+#ifndef SHARK_GSL_UTILS_H
+#define SHARK_GSL_UTILS_H
+
+
+#include <utility>
+
+#include <gsl/gsl_errno.h>
+
+#include "exceptions.h"
+
+namespace shark {
+
+/**
+ * Invokes a GSL function and throws an exception if the return code is anything
+ * other than GSL_SUCESS
+ * @param func The GSL function to invoke
+ * @param args The arguments to pass to the GSL function
+ */
+template<typename Func, typename ... Args>
+void gsl_invoke(Func && func, Args ... args)
+{
+	int status = func(std::forward<Args>(args)...);
+	if (status != GSL_SUCCESS) {
+		throw gsl_error(status, gsl_strerror(status));
+	}
+}
+
+} // namespace shark
+
+#endif // SHARK_GSL_UTILS_H
